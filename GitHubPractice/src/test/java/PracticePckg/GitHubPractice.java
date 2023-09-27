d05a1078e3e065f7fd5cde813563f3524a3c3d0d
rajus <rajus@RajuPC>
2023-09-27 11:26:00 +0530

    SecondCommit

--- GitHubPractice/src/test/java/PracticePckg/GitHubPractice.java
+++ GitHubPractice/src/test/java/PracticePckg/GitHubPractice.java
@@ -3,6 +3,7 @@ package PracticePckg;
 public class GitHubPractice {
 	public static void main(String[] args) {
 		System.out.println("GitHub Practice");
+		System.out.println("GitHub pushing practice");
 	}
 
 }
